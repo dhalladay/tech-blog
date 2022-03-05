0945064e43e216742b38ec0c7a060715a3bcb801
daveh <dave.halladay@outlook.com>
2022-03-04 22:18:12 -0700

    add javascript

---
+++ public/javascript/login.js
@@ -0,0 +1,54 @@
+//allows user to log into existing account
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  //on sign in the users credentials are verifiedr
+  //if successful the user will be directed to the dashboard
+  if (email && password) {
+    const response = await fetch('/api/users/login/', {
+      method: 'post', 
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+//allows user to create a new account 
+async function signupFormHandler(event) {
+  event.preventDefault();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  //collects the nessesary information for the user to create a new account
+  if (username && email && password) {
+    const response = await fetch('/api/users/', {
+      method: 'post', 
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector('#login-btn').addEventListener('submit', loginFormHandler);
+document.querySelector('#signup-btn').addEventListener('submit', signupFormHandler);
